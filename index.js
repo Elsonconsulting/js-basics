84175296dd532b7305993ce2e33d7d611b5fcbe6
Elsonconsulting <97523590+Elsonconsulting@users.noreply.github.com>
2023-01-08 19:19:26 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// For comments add two slashes
+let howOld = 25;
+let firstName = 'Myself';
+// or in an object
+let person = {
+    name: 'Mosh',
+    age: 30
+};
+console.log(person);
+console.log(howOld, firstName);
+
+// Arrays
+let selectColours = ['Red', 'Blue'];
+console.log(selectColours);
+
+// You can see this running in the console for url 'http://127.0.0.1:5500/index.html'
